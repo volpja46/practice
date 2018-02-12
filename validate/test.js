0aec5b54666f92fdeb8bc93fea56c5424409b74c
Jen Volpe <jvolpe721@gmail.com>
2018-02-12 15:55:07 -0500

    added test

--- validate/test.js
+++ validate/test.js
@@ -10,3 +10,14 @@ test("Validate recognizes a valid BST", () => {
 
   expect(validate(n)).toEqual(true);
 });
+
+test("Validate recognizes an invalid BST", () => {
+  const n = new Node(10);
+  n.insert(5);
+  n.insert(15);
+  n.insert(0);
+  n.insert(20);
+  n.left.left.right = new Node(999);
+
+  expect(validate(n)).toEqual(false);
+});
