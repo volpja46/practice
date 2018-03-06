999331679098d0421743bce40c2111ee2cd706be
Jen Volpe <jvolpe721@gmail.com>
2018-03-05 22:34:59 -0500

    more logic

--- expressionOp.js
+++ expressionOp.js
@@ -5,11 +5,15 @@ function expressionOperator(string) {
   // go through expression & see where there are operators
   let addThese = [];
   let subtractThese = [];
-  
+
   for (var i = 0; i < string.length - 1; i++) {
     if (string.includes("+") && !string.includes("-")) {
       let newString = string.replace(/[^\w]/g, "");
       let result = newString.split('').map(function(item) {
           return parseInt(item, 10);
           });
+      let realResult = result.reduce((total, num) => total + num);
+      return realResult
+    }
+  }
 }
