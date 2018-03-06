073b867ff2677a11aee4f48232510d1ef78b6f9b
Jen Volpe <jvolpe721@gmail.com>
2018-03-05 22:34:02 -0500

    ex operator

---
+++ expressionOp.js
@@ -0,0 +1,8 @@
+function expressionOperator(string) {
+  // find where there is a + or - ... then add the following two numbers
+  // add numbers by themselves if they are not one or two places after + or
+  // base case-- if we are at the end of the string-- return the total
+  // go through expression & see where there are operators
+  let addThese = [];
+  let subtractThese = [];
+}
