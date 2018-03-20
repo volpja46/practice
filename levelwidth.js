406d863498a7403697b60ed6e6208c1b8736ebb5
Jen Volpe <jvolpe721@gmail.com>
2018-03-19 23:29:37 -0400

    level wdth 2

--- levelwidth.js
+++ levelwidth.js
@@ -10,3 +10,15 @@
 // |       |
 // 4       5
 // Answer: [1, 3, 2]
+
+
+function levelWidth(root) {
+  // create counter array width of tree @ each levels
+  // create array to hold first node and 'stopper value'
+  const arr = [root, 's'];
+  const counters = [0]
+// iterate thr arr..if 's' is only thing left GET ME OUTTA HERE
+    while (arr.length > 1) {
+    const node = arr.shift()
+
+  }
