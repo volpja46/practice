8aaf616ac9a269cadfbfdbf073f630d57bf90cae
Jen Volpe <jvolpe721@gmail.com>
2018-03-26 23:25:17 -0400

    tests

--- midpoint/test.js
+++ midpoint/test.js
@@ -26,3 +26,20 @@ describe('Midpoint returns the middle node of an odd numbered list', () => {
     expect(midpoint(l).data).toEqual('c');
   });
 });
+describe('Midpoint returns the middle node of an even numbered list', () => {
+  test('when the list has 2 elements', () => {
+    const l = new LinkedList();
+    l.insertLast('a');
+    l.insertLast('b');
+    expect(midpoint(l).data).toEqual('a');
+  });
+
+  test('when the list has 4 elements', () => {
+    const l = new LinkedList();
+    l.insertLast('a');
+    l.insertLast('b');
+    l.insertLast('c');
+    l.insertLast('d');
+    expect(midpoint(l).data).toEqual('b');
+  });
+});
