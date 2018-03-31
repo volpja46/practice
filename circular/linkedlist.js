b93e31e0064dc56108b052c41743825ebf675130
Jen Volpe <jvolpe721@gmail.com>
2018-03-31 15:29:45 -0400

    linked list

---
+++ circular/linkedlist.js
@@ -0,0 +1,31 @@
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor(values = []) {
+    this.head = null;
+
+    for (let value of values) {
+      this.insertLast(value);
+    }
+  }
+
+  clear() {
+    this.head = null;
+  }
+
+  size() {
+    let counter = 0;
+    let node = this.head;
+
+    while (node) {
+      counter++;
+      node = node.next;
+    }
+
+    return counter;
+  }
